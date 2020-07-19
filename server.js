5a1f4573c29e30c2660ed8f5f967fb64d81dc4f2
“Apoorva <apoorvakashyap54@gmail.com>
2020-07-19 20:47:59 +0530

    Dynamic GET Method

--- server.js
+++ server.js
@@ -9,9 +9,18 @@ const mockUserData=[
 app.get('/users', function(req,res){
     res.json({
         success: true,
-        message: 'successfully got users. Nice!',
+        message: 'Successfully got users. Nice!',
         users: mockUserData
     })
+})
+
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
 })
     
 app.listen(8000, function() {
